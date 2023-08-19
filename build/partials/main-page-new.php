861e3484abcb3266c883f163ef77756a43b9a839
user <user@pc>
2023-08-19 06:20:43 +0300

    Главный тулбар

--- build/partials/main-page-new.php
+++ build/partials/main-page-new.php
@@ -3,7 +3,7 @@
     <a href="#" class="img-card" style="background-image: url('<?php print $new['img'];?>')">
       <div class="info">
         <div class="title"><?php print $new['text'];?></div>
-        <div>
+        <div class="datetime">
           <div class="date"><?php print $new['date'];?></div>
           <div class="time"><?php print $new['time'];?></div>
         </div>
